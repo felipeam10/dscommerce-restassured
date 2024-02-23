e5e22d5747278b9569fc5cca2146194e2c010a41
felipeam10 <felipeam10@hotmail.com>
2024-02-23 05:13:58 -0300

    vJSE-6.26 - Test get orders id more scenarios

--- src/test/java/com/felipe/dscommerce/controllers/OrderControllerRA.java
+++ src/test/java/com/felipe/dscommerce/controllers/OrderControllerRA.java
@@ -54,7 +54,79 @@ public class OrderControllerRA {
                 .body("items.name", hasItems("The Lord of the Rings", "Macbook Pro"))
                 .body("total", is(1431.0F))
         ;
-        ;
+    }
 
+    @Test
+    public void findByIdShouldReturnOrderWhenIdExistsAndClientLogged() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/orders/{id}", existingOrderId)
+        .then()
+                .statusCode(200)
+                .body("id", is(1))
+                .body("moment", is("2022-07-25T13:00:00Z"))
+                .body("status", equalTo("PAID"))
+                .body("client.name", equalTo("Maria Brown"))
+                .body("payment.moment", is("2022-07-25T15:00:00Z"))
+                .body("items.size()", is(2))
+                .body("items.name", hasItems("The Lord of the Rings", "Macbook Pro"))
+                .body("total", is(1431.0F))
+        ;
+    }
+
+    @Test
+    public void findByIdShouldReturnForbiddenWhenIdExistsAndClientLoggedAndOrderDoesNotBelongUser() {
+        Long otherOrderId = 2L;
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/orders/{id}", otherOrderId)
+        .then()
+                .statusCode(403)
+        ;
+    }
+
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExistsAndAdminLogged() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + adminToken)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/orders/{id}", nonExistingOrderId)
+        .then()
+                .statusCode(404)
+        ;
+    }
+
+    @Test
+    public void findByIdShouldReturnNotFoundWhenIdDoesNotExistsAndClientLogged() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + clientToken)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/orders/{id}", nonExistingOrderId)
+        .then()
+                .statusCode(404)
+        ;
+    }
+
+    @Test
+    public void findByIdShouldReturnUnauthorizedWhenIdExistsAndInvalidToken() {
+        given()
+                .header("Content-type", "application/json")
+                .header("Authorization", "Bearer " + invalidToken)
+                .accept(ContentType.JSON)
+        .when()
+                .get("/orders/{id}", existingOrderId)
+        .then()
+                .statusCode(401)
+        ;
     }
 }
